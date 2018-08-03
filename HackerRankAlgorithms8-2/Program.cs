61eddfe6e40ee0032ca26802d5655e155679ae66
mikespriggs26 <mikespriggs@msn.com>
2018-08-02 20:53:18 -0400

    First commit

---
+++ HackerRankAlgorithms8-2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankAlgorithms8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 352926151; //# of prisoners
+            int m = 380324688; //# of treats
+            int s = 94730870; //the first chair
+
+            int j = m % n;
+            int finalChair = s - 1 + j;
+            
+
+
+            //int finalChair = (s - 1 + m) % n;
+            //if(finalChair == 0)
+            //{
+            //    finalChair = s;
+            //}
+
+            Console.WriteLine(finalChair);
+
+            Console.ReadLine();
+
+        }
+    }
+}
